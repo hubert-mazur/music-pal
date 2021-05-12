f4302bc40939afc4ecc512399f0806bced1851ec
hubert-mazur <hiusvii@gmail.com>
2021-05-12 17:24:26 +0200

    Project setup

---
+++ src/main/java/com/hm/zti/fis/musicpal/MusicpalApplication.java
@@ -0,0 +1,13 @@
+package com.hm.zti.fis.musicpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicpalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicpalApplication.class, args);
+    }
+
+}
